68a8a64d4755d1bb70beed97ad60b0f83cecac91
冯冬冬 <fengdd@hdsxtech.com>
2017-08-21 14:55:19 +0800

    # add types

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './scss/index.scss'
 import PlotDraw from './core/PlotDraw'
 import PlotEdit from './core/PlotEdit'
+import PlotTypes from './Utils/PlotTypes'
 class olPlot {
   constructor (map) {
     this.plotDraw = new PlotDraw(map)
     this.plotEdit = new PlotEdit(map)
   }
+  static PlotTypes = PlotTypes
 }
 
 export default olPlot
